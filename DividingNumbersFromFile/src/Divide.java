85dea8a6f961e81cf48529fb883815fae31cc884
smehreen <s.mehreenahmed@gmail.com>
2018-03-10 12:04:41 +0600

    Update Divide.java

--- DividingNumbersFromFile/src/Divide.java
+++ DividingNumbersFromFile/src/Divide.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class Divide {
 	public static void main(String[] args) throws Exception {
+		//Edit file path depending on where you keep your input.txt file
 		FileReader file = new FileReader("https://github.com/smehreen/DividingNumbersFromFile-JavaCode-/blob/master/DividingNumbersFromFile/Input.txt");
 		BufferedReader br = new BufferedReader(file);
 		StreamTokenizer st = new StreamTokenizer(br);
